011920ea4761071ae566b4465a85d40f6c492219
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2024-01-12 16:28:04 -0300

    ajuste do formulario e capitura de dados adicionada

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require("body-parser")
+
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -12,6 +17,12 @@ app.get('/', (req, res) => {
 
 app.get('/incluirpergunta', (req, res)  => {
     res.render('perguntar')
+})
+
+app.post('/salvarpergunta', (req, res) =>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("recebido! título: " + titulo + " " + " descrição: " + descricao)
 })
 
 app.listen(3000, ()=>{
