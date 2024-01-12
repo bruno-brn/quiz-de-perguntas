fcb6ba7626be22f1a341bf921f60fc238e83e20d
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2024-01-11 23:41:05 -0300

    adicionado formulario de perguntas

--- index.js
+++ index.js
@@ -4,26 +4,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/:nome/:lang', (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
+app.use(express.static('public'));
 
-    var produtos = [
-        {nome: 'Doritos', preco: 3.14},
-        {nome: 'Fanta', preco: 7.15},
-        {nome: 'Arroz', preco: 2.69},
-    ]
-
-
-    res.render('index',{
-        nome:nome,
-        lang:lang,
-        empresa: 'brn solutions',
-        menssagem: exibirMsg,
-        produtos:produtos
-    });
+app.get('/', (req, res) => {
+    res.render('index')
 });
+
+app.get('/incluirpergunta', (req, res)  => {
+    res.render('perguntar')
+})
 
 app.listen(3000, ()=>{
     console.log('Servidor no ar');
